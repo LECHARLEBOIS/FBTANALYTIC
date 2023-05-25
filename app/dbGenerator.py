36e5256b673cf54aa8a02f46ce353ba7f6665674
LECHARLEBOIS <charleboislouiseric@gmail.com>
2023-05-24 21:18:55 -0400

    2023-05-24

--- app/dbGenerator.py
+++ app/dbGenerator.py
@@ -1,3 +1,23 @@
 import pymysql
+def supplierGenerator ():
+    conn = pymysql.connect(host='localhost', user='root', password='asdf1234', db='PEAKDB')
+    cur = conn.cursor()
 
-conn = pymysql.connect(host='localhost', user='root', password='asdf1234', db='PEAKDB')
+    supplierList = []
+
+    with open("supplierEntry","r") as file:
+        for line in file:
+            supplier, name = line.strip().split(",")
+            query = f"""INSERT INTO supplier (supplierCode, supplierName) VALUES ('{supplier}','{name}');"""
+            cur.execute(query)
+            conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+def DBGeneration ():
+    supplierGenerator()
+
+
+DBGeneration()
